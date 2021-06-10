3b22415bf0dc4e1756d8de95b6a029b4803e49e5
JGabriel-SL <gabrielsl.alu.lmb@gmail.com>
2021-06-10 09:55:19 -0300

    adding the function for groupWords and show data

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,18 +1,13 @@
 const { ipcMain } = require('electron')
 const pathsToRows = require('./pathsToRows')
 const prepareData = require('./prepareData')
+const groupWords = require('./groupWords')
 
 ipcMain.on('process-subtitle', (event, paths) => {    
     pathsToRows(paths)
         .then(rows => prepareData(rows))
-        .then(words => console.log(words))
-        .then(() => {
-            event.reply('process-subtitle', [
-                { name: 'i', amount: 900 },
-                { name: 'you', amount: 870 },
-                { name: 'he', amount: 710 },
-                { name: 'she', amount: 544 },
-                { name: 'our', amount: 123 }
-            ])
+        .then(words => groupWords(words))
+        .then(groupedWords => {
+            event.reply('process-subtitle', groupedWords)
         })
 })
